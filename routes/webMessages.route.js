bbd648863f623df4bde76ccbeba2e488676e41e5
TariqAli93 <tariqali9300@gmail.com>
2025-08-02 14:43:30 +0300

    edit the webMessageRoute from api to web

--- routes/webMessages.route.js
+++ routes/webMessages.route.js
@@ -2,17 +2,17 @@ import * as webMessagesController from "../controllers/webMessages.controller.js
 
 const webMessagesRouter = (app) => {
   // getWebMessages
-  app.get("/api/webMessages", webMessagesController.getWebMessages);
+  app.get("/web/webMessages", webMessagesController.getWebMessages);
   // getWebMessageById
-  app.get("/api/webMessages/:id", webMessagesController.getWebMessageById);
+  app.get("/web/webMessages/:id", webMessagesController.getWebMessageById);
   // markAllMessagesAsRead
-  app.put("/api/webMessages/read", webMessagesController.markAllMessagesAsRead);
+  app.put("/web/webMessages/read", webMessagesController.markAllMessagesAsRead);
   // markAllMessagesAsDeleted
-  app.delete("/api/webMessages/delete", webMessagesController.markAllMessagesAsDeleted);
+  app.delete("/web/webMessages/delete", webMessagesController.markAllMessagesAsDeleted);
   // createWebMessage
-  app.post("/api/webMessages", webMessagesController.createWebMessage);
+  app.post("/web/webMessages", webMessagesController.createWebMessage);
   // deleteWebMessage
-  app.delete("/api/webMessages/:id", webMessagesController.deleteWebMessage);
+  app.delete("/web/webMessages/:id", webMessagesController.deleteWebMessage);
 };
 
 export default webMessagesRouter;
