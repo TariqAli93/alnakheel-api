852787bc4826ffd8180b8ea2bc02415901afaeeb
TariqAli93 <tariqali9300@gmail.com>
2025-08-06 15:34:20 +0300

    try to upload images

--- controllers/images.controllers.js
+++ controllers/images.controllers.js
@@ -2,6 +2,16 @@ import * as imageModel from '../models/images.model.js';
 import uploadService from '../services/upload.service.js';
 
 export const saveImage = async (req, res, next) => {
-  console.log('saveImage called');
+  console.log('saveImage called', req.file);
+  try {
+    const image = await uploadService.uploadImage(req.file);
+    res.status(200).json({
+      message: 'Image saved successfully',
+      image,
+      success: true,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
