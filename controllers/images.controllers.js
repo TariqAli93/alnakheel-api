9693cc7ed947b599f214c4d9795a975bdd7e1f42
TariqAli93 <tariqali9300@gmail.com>
2025-08-06 15:22:41 +0300

    check save images

--- controllers/images.controllers.js
+++ controllers/images.controllers.js
@@ -2,7 +2,6 @@ import * as imageModel from '../models/images.model.js';
 import uploadService from '../services/upload.service.js';
 
 export const saveImage = async (req, res, next) => {
-
   console.log('saveImage called');
 };
 
