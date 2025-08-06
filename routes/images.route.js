8cf74ae91049733e672193383b00d69184c6c8b3
TariqAli93 <tariqali9300@gmail.com>
2025-08-06 15:17:37 +0300

    check save images

--- routes/images.route.js
+++ routes/images.route.js
@@ -3,8 +3,6 @@ import * as imagesController from '../controllers/images.controllers.js';
 const imagesRouter = (app) => {
   // Route to save an image
   app.post('/images/upload', imagesController.saveImage);
-  // Route to delete an image by ID
-  app.delete('/images/:id', imagesController.deleteImage);
 };
 
 export default imagesRouter;
