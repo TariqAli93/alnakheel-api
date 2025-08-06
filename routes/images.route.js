085d46d8b5557a477176b6699d8a4d7f430be1eb
TariqAli93 <tariqali9300@gmail.com>
2025-08-06 15:19:54 +0300

    check save images

--- routes/images.route.js
+++ routes/images.route.js
@@ -1,7 +1,6 @@
 import * as imagesController from '../controllers/images.controllers.js';
 
 const imagesRouter = (app) => {
-  // Route to save an image
   app.post('/images/upload', imagesController.saveImage);
 };
 
