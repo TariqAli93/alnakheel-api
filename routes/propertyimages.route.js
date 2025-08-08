e54edd05b2473255707944d2e199bd49433bf9f4
TariqAli93 <tariqali9300@gmail.com>
2025-08-08 12:19:01 +0300

    edit propertyimage route

--- routes/propertyimages.route.js
+++ routes/propertyimages.route.js
@@ -1,10 +1,9 @@
 import * as propertyImagesController from '../controllers/propertyimages.controller.js';
 
 const propertyimagesRouter = (app) => {
-  // app.post('/api/propertyimages', propertyImagesController.createPropertyImage);
+  app.post('/api/propertyimages', propertyImagesController.createPropertyImage);
   app.get('/api/propertyimages', propertyImagesController.getPropertyImages);
   app.delete('/api/propertyimages/:propertyId/:imageId', propertyImagesController.deletePropertyImage);
-  app.post('/images/upload', propertyImagesController.createPropertyImage);
 }
 
 export default propertyimagesRouter;
